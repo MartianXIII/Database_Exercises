7739b567c7759f0952509f7e80c806ef2c65b522
Mars <marianomarquis@gmail.com>
2015-07-30 10:37:15 -0500

    SECOND

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -2,3 +2,9 @@ SELECT DISTINCT last_name
 FROM employees
 ORDER BY last_name DESC
 LIMIT 10;
+
+SELECT concat(first_name, ' ', last_name) AS 1990s_hires
+FROM employees
+WHERE hire_date LIKE '199%' AND (birth_date LIKE '%-12-25')
+ORDER BY birth_date ASC, hire_date DESC
+LIMIT 5;
