a2cd3bf7eb25afa19865c802b2ef56a356ef94dc
Mars <marianomarquis@gmail.com>
2015-07-30 10:39:41 -0500

    THIRD

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -8,3 +8,9 @@ FROM employees
 WHERE hire_date LIKE '199%' AND (birth_date LIKE '%-12-25')
 ORDER BY birth_date ASC, hire_date DESC
 LIMIT 5;
+
+SELECT concat(first_name, ' ', last_name) AS 1990s_hires
+FROM employees
+WHERE hire_date LIKE '199%' AND (birth_date LIKE '%-12-25')
+ORDER BY birth_date ASC, hire_date DESC
+LIMIT 5 OFFSET 45;
