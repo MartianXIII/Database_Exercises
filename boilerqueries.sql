a31c0496b80474baac6cba5bef0e2689f59cf5a5
Mars <marianomarquis@gmail.com>
2016-01-27 12:31:06 -0600

    CheatSchema

---
+++ boilerqueries.sql
@@ -0,0 +1,26 @@
+    -- MYSQL-BOILER QUERIES
+
+    -- Connecting in Terminal
+
+
+
+
+      -- QUERY
+  SELECT * FROM table
+  SELECT * FROM table1, table2, ...
+  SELECT field1, field2, ... FROM table1, table2, ...
+
+  SELECT ... FROM ... WHERE condition
+  SELECT ... FROM ... WHERE condition GROUP BY field
+  SELECT ... FROM ... WHERE condition GROUP BY field HAVING condition2
+  SELECT ... FROM ... WHERE condition ORDER BY field1, field2
+  SELECT ... FROM ... WHERE condition ORDER BY field1, field2 DESC
+  SELECT ... FROM ... WHERE condition LIMIT 10
+
+  SELECT DISTINCT field1 FROM ...
+  SELECT DISTINCT field1, field2 FROM ...
+
+  SELECT ... FROM t1 JOIN t2 ON t1.id1 = t2.id2 WHERE condition
+  SELECT ... FROM t1 LEFT JOIN t2 ON t1.id1 = t2.id2 WHERE condition
+  SELECT ... FROM t1 JOIN (t2 JOIN t3 ON ...) ON ...
+  SELECT ... FROM t1 JOIN t2 USING(id) WHERE condition
